e4613b8d077f6067cdfca0953c5c1624650ae230
Shridhar600 <65012105+Shridhar600@users.noreply.github.com>
2022-10-14 01:41:06 +0530

    oct13

---
+++ loops.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    //for(initialisation ; condition; update)
+    int sum = 0;
+    for(int i = 1; i <= n; i++){
+        cout << "Hello"<< endl;
+    }
+
+    //while(condition is true )
+    int j = 1;
+
+    while( j <= n){
+        cout<<"While wala hello"<< endl;
+        j++;
+    }
+    return 0;
+    
+}
