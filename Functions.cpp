0846dd9c59d282dbb6d3aafe6a0f094e23f7f534
Shridhar600 <65012105+Shridhar600@users.noreply.github.com>
2022-10-28 01:21:10 +0530

    functions

---
+++ Functions.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+
+//FUNCTION:
+
+// returnType Function name (parameter1, parameter2, ......){
+    //body 
+//  }
+
+//function to add two numbers.
+
+
+int function(int a, int b){
+    int sum;
+    sum = a + b;
+    return sum;
+}
+
+int main(){
+
+    int a = 2;
+    int b = 3;
+
+    cout << function(a,b) ;
+
+    return 0;
+
+}
+
