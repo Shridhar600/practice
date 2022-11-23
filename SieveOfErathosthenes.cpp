2fcdfd1fe7823e1f62ce40629e80802c9f974dd4
Shridhar600 <65012105+Shridhar600@users.noreply.github.com>
2022-11-24 04:04:36 +0530

    sieve of eratosthenes NOT COMPLETE

---
+++ SieveOfErathosthenes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+// all the prime numbers between a range.
+void primeSieve(int n){
+
+    int arr[100] = {0};
+
+    for(int i = 2; i <=n; i++){
+        if(arr[])
+    }
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    
+    
+
+return 0;
+}
