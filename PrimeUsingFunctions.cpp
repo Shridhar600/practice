e1006a330a525f0b9fa6aa0485f77e620c7a1f36
Shridhar600 <65012105+Shridhar600@users.noreply.github.com>
2023-01-03 17:19:27 +0530

    Revision

--- PrimeUsingFunctions.cpp
+++ PrimeUsingFunctions.cpp
@@ -1,23 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool isPrime(int num){
-    for(int j = 2; j < num; j++){
-        if(num % j == 0){
+bool isPrime(int num)
+{
+    for (int j = 2; j < num; j++)
+    {
+        if (num % j == 0)
+        {
             return false;
         }
     }
     return true;
 }
 
+int main()
+{
 
-int main(){
-
-    int a,b;
+    int a, b;
     cin >> a >> b;
 
-    for(int i = a ; i <=b ; i++){
-        if(isPrime(i)){
+    for (int i = a; i <= b; i++)
+    {
+        if (isPrime(i))
+        {
             cout << i << endl;
         }
     }
--- recursionQuest.cpp
+++ recursionQuest.cpp
@@ -1,20 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
- 
-void printHello(int n){
 
-    if(n ==0){
+void printHello(int n)
+{
+
+    if (n == 0)
+    {
         return;
     }
-    cout << "Hello"<< endl;
-    printHello(n-1);
+    cout << "Hello" << endl;
+    printHello(n - 1);
 }
 
-int main(){
+int main()
+{
     int n;
-    cin >>n;
+    cin >> n;
 
-    printHello(n); 
- 
-return 0;
+    printHello(n);
+
+    return 0;
 }
