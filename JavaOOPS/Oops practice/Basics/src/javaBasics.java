60d70c8a8cbedf6c66017b82fb326582bd156e9a
Shridhar600 <65012105+Shridhar600@users.noreply.github.com>
2023-02-20 05:26:37 +0530

    basic

---
+++ JavaOOPS/Oops practice/Basics/src/javaBasics.java
@@ -0,0 +1,12 @@
+public class javaBasics {
+     public static void main(String[] args){
+      int a =22;
+      while(true){
+          ++a;
+          System.out.println(a);
+        if(a>50){
+            break;
+        }
+      }  
+     }
+}
