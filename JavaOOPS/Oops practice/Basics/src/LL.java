7f242a802d66b74cfa67d306144e08e0cf6bfc36
Shridhar600 <65012105+Shridhar600@users.noreply.github.com>
2023-02-24 18:30:01 +0530

    Basic structure of LinkedList fml

---
+++ JavaOOPS/Oops practice/Basics/src/LL.java
@@ -0,0 +1,53 @@
+public class LL {
+    public static void main(String[] args) {   
+        LinkedList list1 = new LinkedList();
+        list1.addLast(5);
+        list1.addLast(4);
+        list1.addLast(3);
+        list1.addLast(2);
+        list1.addLast(1);
+
+        list1.printList();
+    }
+}
+
+class Node{
+    int data;
+    Node next;
+
+    // constructor 
+    public Node(int data){
+        this.data = data;
+        this.next = null; // intial value of all the new nodes created.
+    }
+}
+
+class LinkedList{
+    Node head;
+    // constructor 
+    public LinkedList(){
+        this.head = null; // MEANS ABHI KOI VALUE NAHI HAI- KAHLI LINKED LIST HAI.
+    }
+    public void addLast(int data){
+        Node newNode = new Node(data);
+
+        // check if there is only one node.
+        if(head == null){
+            head = newNode;
+        }else {
+            //traversal 
+            Node current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    public void printList(){
+        Node current = head;
+        while(current !=null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+}
